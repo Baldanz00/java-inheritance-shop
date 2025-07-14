2215bd6bb7cc17456ce5e84e1c02def05ffc4d58
Baldanz00 <gioia.travaglini18@gmail.com>
2025-07-14 13:38:58 +0200

    Update Main.java

--- jva-inheritance-shop/Main.java
+++ jva-inheritance-shop/Main.java
@@ -1,6 +1,6 @@
 public class Main {
 public static void main(String[] args) {
-    Smartphone s1 = new Smartphone("152689427", "Iphone15", "Apple", 1200.99, 22 , "125879645", 256);
+    Smartphone s1 = new Smartphone(152689427, "Iphone15", "Apple", 1200.99, 22 , "125879645", 256);
     System.out.print(s1.getDettagliProdotto());
 
     Televisori t1 = new Televisori(55, true);
