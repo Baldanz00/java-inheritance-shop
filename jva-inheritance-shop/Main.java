3a991bba9cd3f6bb24ea549ae83a2f61f8cc956c
Baldanz00 <gioia.travaglini18@gmail.com>
2025-07-14 20:35:34 +0200

    Correzione main per errore di salvataggio file sbagliato

--- jva-inheritance-shop/Main.java
+++ jva-inheritance-shop/Main.java
@@ -1,14 +1,8 @@
 public class Main {
 public static void main(String[] args) {
-    Smartphone s1 = new Smartphone(152689427, "Iphone15", "Apple", 1200.99, 22 , "125879645", 256);
-<<<<<<< HEAD
-    System.out.println(s1);
-=======
-    System.out.print(s1.getDettagliProdotto());
 
-    Televisori t1 = new Televisori(55, true);
-    System.out.println(t1.getDettagliProdotto());
->>>>>>> 2215bd6bb7cc17456ce5e84e1c02def05ffc4d58
+    Smartphone s1 = new Smartphone(152689427, "Iphone15", "Apple", 1200.99, 22 , "125879645", 256);
+    System.out.println(s1);
 
     Televisori t1 = new Televisori(256847, "Samsung Oled", "Samsung", 15499.99, 22, 55, true);
     System.out.print(t1);
