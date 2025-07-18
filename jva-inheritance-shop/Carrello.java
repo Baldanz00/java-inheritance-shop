329d64fba853a7dc491e21bbceb0c67cf177ec0b
Baldanz00 <gioia.travaglini18@gmail.com>
2025-07-18 08:53:40 +0200

    prima bozza carrello

---
+++ jva-inheritance-shop/Carrello.java
@@ -0,0 +1,19 @@
+//Create una classe Carrello con metodo main, 
+//in cui chiedete all’utente di valorizzare un carrello 
+//di prodotti con dati inseriti tramite scanner.
+//Durante la richiesta di valorizzazione chiedete all’utente se 
+//sta inserendo uno Smarphone o un Televisore o Cuffie e in base
+// alla scelta dell’utente utilizzate il costruttore opportuno.
+//Al termine dell’inserimento stampate il carrello
+// (fate l’override del metodo toString per restituire le 
+//informazioni da stampare per ogni classe)
+
+import java.util.Scanner;
+public class Carrello extends Prodotto{
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+//nome,codice,marca,prezzo,iva
+        System.out.print("Inserisci il nome del prodotto: ");
+    }
+
+}
